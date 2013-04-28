a64436107d0e8380a64f5f3485cb5f9d3d7535cf
zweinC <zweinC@gmail.com>
2013-04-28 13:44:11 +0800

    Finish sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130418013139) do
+ActiveRecord::Schema.define(:version => 20130427025427) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,8 +19,12 @@ ActiveRecord::Schema.define(:version => 20130418013139) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remeber_token"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remeber_token"], :name => "index_users_on_remeber_token"
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
